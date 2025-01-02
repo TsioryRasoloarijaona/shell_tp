5e7ec19945178f33e989aa152ec641813d71d793
TsioryRasoloarijaona <hei.tsiory@gmail.com>
2025-01-02 11:28:54 +0100

    adding if else condition script

---
+++ 3isi_then.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+if [ $# -ne 0 ]
+then
+	echo "$# parametre ligne de commande"
+else
+	echo "aucun paremetre; set salim siham omar"
+	set asalim sahim omar
+fi
+
+echo "nombre de parametre : 1=$1 2=$2 3=$3"
+
+echo "liste : $*"
+
+ 
